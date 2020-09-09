1a6144ba2dd33ea246b9ec3dc6b92032cbb90a82
Travis Wood <travis.230262@nv.ccsd.net>
2020-09-09 18:55:36 +0000

    Actually fixed desks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -17,15 +17,31 @@ class StudentDesks {
        /*
         *  your code goes here
         */
-        stu = cA + cB + cC;
-        if(stu % 2 == 0)
+        if(cA % 2 == 0)
         {
-         stu = stu / 2;
+         cA = cA / 2;
         }else
         {
-         stu = stu + 1;
-         stu = stu / 2;
+         cA = cA + 1;
+         cA = cA / 2;
         }
+         if(cB % 2 == 0)
+        {
+         cB = cB / 2;
+        }else
+        {
+         cB = cB + 1;
+         cB = cB / 2;
+        }
+        if(cC % 2 == 0)
+        {
+         cC = cC / 2;
+        }else
+        {
+         cC = cC + 1;
+         cC = cC / 2;
+        }
+        stu = cA + cB + cC;
         int desk = stu;
         System.out.print(desk);
  
