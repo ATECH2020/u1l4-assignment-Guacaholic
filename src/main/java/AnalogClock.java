6466036ecfee1d6a91dbd3b588b842509d3db541
Travis Wood <travis.230262@nv.ccsd.net>
2020-09-09 16:40:22 +0000

    Fixed fork and moved code to here.

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
-
+ 
 class AnalogClock {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
-
-
-        /*
-         *  your code goes here
-         */
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads ints from user
+       int ang = scanner.nextInt();
+       int hour;
+       int degrees;
+       int time;
+ 
+       /*
+        *  your code goes here
+        */
+       hour = ang / 30;
+       degrees = ang - (hour * 30);
+       time = degrees * 2;
+       System.out.print(time);
+ 
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
-
+ 
 class StudentDesks {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
-        
-
-        /*
-         *  your code goes here
-         */
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads ints from user
+       int cA = scanner.nextInt();
+       int cB = scanner.nextInt();
+       int cC = scanner.nextInt();
+       int stu;
+ 
+      
+ 
+       /*
+        *  your code goes here
+        */
+        stu = cA + cB + cC;
+        if(stu % 2 == 0)
+        {
+         stu = stu / 2;
+        }else
+        {
+         stu = stu + 1;
+         stu = stu / 2;
+        }
+        int desk = stu;
+        System.out.print(desk);
+ 
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
